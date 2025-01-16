b21ca5f3acf8bb4a374fa3317b0e6e35d9e6f607
Myllena Bitar <111917539+myllenabitar@users.noreply.github.com>
2025-01-16 17:42:57 -0300

    add index.html and normalize text functions in script.js and script.ts

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 "use strict";
-const produto = "Livro";
-const preco = 200;
+let produto = "Livro";
+let preco = 200;
 const carro = {
     marca: 'Audi',
     portas: 5
 };
+const barato = preco < 400 ? true : "produto caro";
+function somar(a, b) {
+    return a + b;
+}
+somar(4, 10);
+//somar('2', 4); // erro 
+const nintendo = {
+    nome: 'Nintendo',
+    preco: '2000',
+};
+function transformarPreco(produto) {
+    produto.preco = 'R$' + produto.preco;
+    return produto;
+}
+const ProdutoNovo = transformarPreco(nintendo);
+document.body.innerText = JSON.stringify(ProdutoNovo);
--- script.ts
+++ script.ts
@@ -1,10 +1,33 @@
-const produto: string = "Livro";
-const preco: number =  200;
+let produto = "Livro";
+let preco =  200;
 
-const carro :{
-    marca: string;
-    portas: number;
-}= {
+const carro = {
     marca: 'Audi',
     portas: 5
+};
+
+const barato: boolean | string = preco < 400 ? true : "produto caro"; 
+
+function somar (a: number, b: number) {
+    return a + b
 }
+
+
+somar (4, 10);
+//somar('2', 4); // erro 
+
+
+const nintendo = {
+    nome:'Nintendo', 
+    preco: '2000',  
+}
+
+function transformarPreco (produto: {nome: string; preco: string;}) {
+    produto.preco = 'R$' + produto.preco; 
+    return produto;
+}
+
+const ProdutoNovo = transformarPreco(nintendo);
+
+document.body.innerText = JSON.stringify(ProdutoNovo);
+
