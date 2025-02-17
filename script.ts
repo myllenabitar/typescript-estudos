13c0742301746acd5b52f6b61bf53a4f05dd318c
Myllena Bitar <111917539+myllenabitar@users.noreply.github.com>
2025-02-17 19:28:18 -0300

    docs: add exercise instructions for fetching and processing sales data in script.ts

--- script.ts
+++ script.ts
@@ -202,4 +202,10 @@ async function puxaDados(url:string) {
   <label for="cpf">CPF</label>
   <input type="text" id="cpf" name="cpf" />
 </form>
+
+
+// EXERCICIOS
+// 1 - Faça um fetch das vendas: https://api.origamid.dev/json/vendas.json
+// 2 - Defina o tipo/interface de cada venda (tuple)
+// 3 - Some o total das vendas e mostre na tela
 */
